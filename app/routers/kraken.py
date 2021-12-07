ed007aa52691e007e7710dafa241b0661a3aaa4d
terrotar <pedro_guedes93@hotmail.com>
2021-12-06 22:19:28 -0300

    Added folder Documents with project's scope; Added coin Ethereum in /kraken/orderbook

--- app/routers/kraken.py
+++ app/routers/kraken.py
@@ -16,8 +16,13 @@ router = APIRouter(prefix='/kraken',
 async def kraken_order_book():
     order_book = httpx.get('https://api.kraken.com/0/public/Depth?pair=XBTUSD&count=1')
     bitcoin_ask = order_book.json()['result']['XXBTZUSD']['asks'][0][0]
+    order_book = httpx.get('https://api.kraken.com/0/public/Depth?pair=XETHZUSD&count=1')
+    ethereum_ask = order_book.json()['result']['XETHZUSD']['asks'][0][0]
     return {
         'BTC': {
             'Price(USD)': float(bitcoin_ask)
+        },
+        'ETH': {
+            'Price(USD)': float(ethereum_ask)
         }
     }
