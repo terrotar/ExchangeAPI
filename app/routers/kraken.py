3e2fc4e6646eab2edd463ce0093b17cdef9ad152
terrotar <pedro_guedes93@hotmail.com>
2021-12-07 00:00:56 -0300

    Added price in USD and BRL in both kraken and mercado_bitcoin

--- app/routers/kraken.py
+++ app/routers/kraken.py
@@ -14,15 +14,24 @@ router = APIRouter(prefix='/kraken',
 # Get Order Book
 @router.get('/orderbook')
 async def kraken_order_book():
+
+    # Get cryptocoins in USD
     order_book = httpx.get('https://api.kraken.com/0/public/Depth?pair=XBTUSD&count=1')
-    bitcoin_ask = order_book.json()['result']['XXBTZUSD']['asks'][0][0]
+    bitcoin_ask = float(order_book.json()['result']['XXBTZUSD']['asks'][0][0])
     order_book = httpx.get('https://api.kraken.com/0/public/Depth?pair=XETHZUSD&count=1')
-    ethereum_ask = order_book.json()['result']['XETHZUSD']['asks'][0][0]
+    ethereum_ask = float(order_book.json()['result']['XETHZUSD']['asks'][0][0])
+
+    # Convert to BRL
+    exchange_usd_brl = httpx.get('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+    usd_brl = float(exchange_usd_brl.json()['USDBRL']['ask'])
+
     return {
         'BTC': {
-            'Price(USD)': float(bitcoin_ask)
+            'Price(USD)': round(bitcoin_ask, 2),
+            'Price(BRL)': round(bitcoin_ask*usd_brl, 2)
         },
         'ETH': {
-            'Price(USD)': float(ethereum_ask)
+            'Price(USD)': round(ethereum_ask, 2),
+            'Price(BRL)': round(ethereum_ask*usd_brl, 2)
         }
     }
--- app/routers/mercado_bitcoin.py
+++ app/routers/mercado_bitcoin.py
@@ -13,20 +13,24 @@ router = APIRouter(prefix='/mercado_bitcoin',
 # Get Order Book
 @router.get('/orderbook')
 async def mercado_bitcoin_order_book():
+
+    # Get cryptocoins in USD
     order_book = httpx.get('https://www.mercadobitcoin.net/api/BTC/orderbook/')
     bitcoin_ask = round(order_book.json()['asks'][0][0], 2)
     order_book = httpx.get('https://www.mercadobitcoin.net/api/ETH/orderbook/')
     ethereum_ask = round(order_book.json()['asks'][0][0], 2)
 
-    # Get USD/BRL
+    # Convert to BRL
     exchange_usd_brl = httpx.get('https://economia.awesomeapi.com.br/json/last/USD-BRL')
     usd_brl = float(exchange_usd_brl.json()['USDBRL']['ask'])
 
     return {
         'BTC': {
-            'Price(USD)': round(bitcoin_ask/usd_brl, 2)
+            'Price(USD)': round(bitcoin_ask/usd_brl, 2),
+            'Price(BRL)': round(bitcoin_ask, 2)
         },
         'ETH': {
-            'Price(USD)': round(ethereum_ask/usd_brl, 2)
+            'Price(USD)': round(ethereum_ask/usd_brl, 2),
+            'Price(BRL)': round(ethereum_ask, 2)
         }
     }
